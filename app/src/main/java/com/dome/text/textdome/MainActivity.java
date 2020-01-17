2303073e37af17a973033aec82efd6d1fe03079c
lucky686868 <1515858918@qq.com>
2020-01-17 11:18:36 +0800

    测试

--- app/src/main/java/com/dome/text/textdome/MainActivity.java
+++ app/src/main/java/com/dome/text/textdome/MainActivity.java
@@ -1,7 +1,9 @@
+
 package com.dome.text.textdome;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,6 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Toast.makeText(MainActivity.this,"哈喽",Toast.LENGTH_SHORT).show();
     }
 }
